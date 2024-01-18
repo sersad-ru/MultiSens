114ee99f6da63a73c1b0a69cb44967256a9c9559
sersad <sersad@gmail.com>
2024-01-18 18:53:37 +0300

    Update plgQRE1113.h

--- plgQRE1113.h
+++ plgQRE1113.h
@@ -11,7 +11,6 @@
 * 
 * Results are printed on the screen and sends to serial in human readable format.* 
 * Use UP and DOWN buttons to change poll delay. Avaible values are 10, 50, 100, 250, 500, 1000, 1500 and 2000 ms
-* Current delay is displayed after P6 value (use LEFT and RIGTH buttuns to scrool the screen)
 * Press and hold SELECT button to store current delay in EEPROM.
 * Hold DOWN button and pres RESET to clear stored value and return to defaults. (ATTENTION! All stored data will be cleared. FOR ALL plugins!)
 *  
